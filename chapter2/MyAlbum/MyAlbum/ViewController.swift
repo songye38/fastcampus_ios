88751e49f0f67613922910dcc9b272320cb802ce
songye38 <ddev0308@gmail.com>
2021-03-24 15:21:17 +0900

    add button and alert action

---
+++ chapter2/MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by songye on 2021/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(_ sender: Any) {
+        let alert = UIAlertController(title:"hello", message: "My First App", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title : "ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    
+}
+
+}
