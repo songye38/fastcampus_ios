a53c87caa7dffe02f58bd9cadbbc2014489fba8f
songye38 <ddev0308@gmail.com>
2021-03-25 14:28:52 +0900

    add outlet

--- chapter2/MyAlbum/MyAlbum/ViewController.swift
+++ chapter2/MyAlbum/MyAlbum/ViewController.swift
@@ -8,17 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue = 0
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        priceLabel.text = "\(currentValue)"
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title:"hello", message: "My First App", preferredStyle: .alert)
+        let message = "가격은 \(currentValue) 입니다"
+        let alert = UIAlertController(title:message, message: "My First App", preferredStyle: .alert)
         
         let action = UIAlertAction(title : "ok", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        let randomPrice = arc4random_uniform(10000)+1
+        currentValue = Int(randomPrice)
+        priceLabel.text = "\(currentValue)"
 }
 }
