128a5e39ddb1714cae5cdc89a450800616216ae3
songye38 <ddev0308@gmail.com>
2021-03-29 19:15:32 +0900

    default function

--- chapter5/function_study.playground/Contents.swift
+++ chapter5/function_study.playground/Contents.swift
@@ -29,17 +29,31 @@ printMultipleOfTen(value: 10)
 
 //param2개
 //물건값과 개수를 받아서 전체 금액을 출력하는 함수
-//func printTotalPrice(price : Int, count : Int){
-//    print("total value is \(price * count)")
-//}
+func printTotalPrice(price : Int, count : Int){
+    print("total value is \(price * count)")
+}
 
 //func printTotalPrice(_ price : Int, _ count : Int){
 //    print("total value is \(price * count)")
 //}
 
-func printTotalPrice(가격 price : Int, 갯수 count : Int){
+//func printTotalPrice(가격 price : Int, 갯수 count : Int){
+//    print("total value is \(price * count)")
+//}
+
+//default 값을 세팅해주기
+func printTotalPriceDefault(price : Int = 1500, count : Int){
     print("total value is \(price * count)")
 }
 
-printTotalPrice(가격: 1000, 갯수: 30)
-//printTotalPrice(1500,5)
+printTotalPrice(price: 1000, count: 30)
+printTotalPriceDefault(count : 5)
+printTotalPriceDefault(price : 2000, count : 5)
+
+//값을 반환해주기
+func returnPrice(price : Int, count : Int) -> Int{
+    return price * count
+}
+
+var number = returnPrice(price : 3000, count : 2)
+print(number)
