8f217cce25f04caef0025c8ca6ccef324b4dd5e1
songye38 <ddev0308@gmail.com>
2021-03-31 15:12:26 +0900

    study set type

---
+++ chapter6/set.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+
+var someArray :Array<Int> = [1,2,3,4]
+var someSet : Set<Int> = [1,2,3,4,4]
+print(someSet)
+
+someSet.isEmpty
+someSet.count
+someSet.contains(4)
+
+someSet.insert(5)
+someSet.remove(1)
