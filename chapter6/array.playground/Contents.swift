35d696a78de431c65a71830ea7335403a764ab43
songye38 <ddev0308@gmail.com>
2021-03-31 15:07:38 +0900

    study dictionary type

--- chapter6/array.playground/Contents.swift
+++ chapter6/array.playground/Contents.swift
@@ -1,97 +1,75 @@
 import UIKit
 
 
-//how to make array in Swift
-var evenNumbers : [Int] = [2,4,6,8]
-let evenNumbers2 : Array<Int> = [2,4,6,8]
+//how to make dictionary in Swift
+var scoreDic : [String : Int] = ["Jason":80 , "Jay" : 95, "Jake" : 90]
+var scoreDic2 : Dictionary<String,Int> = ["Jason" : 80 , "Jay" : 95, "Jake" : 90]
 
-//how to append values to array
-evenNumbers.append(10)
-evenNumbers += [12,14,16]
-evenNumbers.append(contentsOf: [18,20])
 
-//can check if array is empty or not
-let isEmpty = evenNumbers.isEmpty
-
-//check how many elements in an array?
-let count = evenNumbers.count
-
-//check the first element in an array
-let firstItem = evenNumbers.first //int optional로 반환
-let lastItem = evenNumbers.last //can be an item or nil
-
-//use optional with array.first
-if let firstElement = evenNumbers.first {
-    print("first item is \(firstElement)")
+if let score = scoreDic["Jason"] {
+    print(score)
 }else{
-    print("nil")
+    //score 없음
+}
+scoreDic["Jason"]
+scoreDic["Joon"]
+
+//isEmpty, count
+//scoreDic = [:] //빈 딕셔너리로 만들기
+scoreDic.isEmpty
+scoreDic.count
+
+//기존 사용자 업데이트
+scoreDic["Jason"] = 99
+scoreDic
+
+//사용자 추가
+scoreDic["Jeck"] = 100
+
+//사용자 제거
+scoreDic["Jeck"] = nil
+scoreDic
+
+//아이템 빼내기
+for (name, score) in scoreDic {
+    print("\(name), \(score)")
 }
 
-//get min and max value in an array return type is int optional type
-evenNumbers.min()
-evenNumbers.max()
-
-//get an item with index in an array
-var tenthItem = evenNumbers[9]
-//var twntithItem = evenNumbers[19] //fatal error
-
-let firstThree = evenNumbers[0...2] //get 0,1,2 using range
-
-//can check if there is specific value in an array
-evenNumbers.contains(3)
-evenNumbers.contains(4)
-
-//특정 인덱스에 원하는 값을 삽입할 수도 있음
-evenNumbers.insert(0,at :0)
-evenNumbers
-
-//remove all elemenets
-//evenNumbers.removeAll()
-//evenNumbers = []
-
-//remove specific elements
-evenNumbers.remove(at: 0)
-evenNumbers[0] = -2
-evenNumbers[0...2] = [-2,0,2]
-
-//swap using index
-evenNumbers.swapAt(0,9)
-
-//looping
-
-//for num in evenNumbers {
-//    print(num)
-//}
-
-for (index,num) in evenNumbers.enumerated(){
-    print(index,num)
+for key in scoreDic.keys {
+    print(key)
 }
 
-//앞의 세개를 없애는 함수
-let numbers = evenNumbers.dropFirst(3) //원래것은 건들이지 않고 세개를 없애고 새로운 배열을 반환해준다.
-evenNumbers
-numbers
+print("hello")
 
-let lastNumebrs = evenNumbers.dropLast(1)
-lastNumebrs
+//1. 이름, 직업 , 도시에 대해서 본인의 딕셔너리 만들기
+// 2. 도시를 부산으로 업데이트 하기
+//3. 딕셔너리 받아서 이름과 도시 프린트 하는 함수 만들기
 
-//앞에 세개만 가져오고 싶을 때
-let fistItems = evenNumbers.prefix(3)
-fistItems
+var infoDic : [String:String] = ["name" : "songye", "job":"programmer", "city":"namyangju"]
+infoDic["city"] = "Busan"
 
-let lastItems = evenNumbers.suffix(3)
-lastItems
+func printInfo(){
+    if let _ = infoDic["name"]{
+        print("name : \(infoDic["name"])")
+    }else{
+        print("no name")
+    }
+    //print("name : \(infoDic["name"]), city : \(infoDic["city"])")
+    
+}
 
+printInfo()
 
-//꼭 알아야 하 것들
+var myDic : [String:String] = ["name":"songye", "job": "ios Engineer", "city":"seoul"]
+myDic["city"] = "Busan"
+myDic
 
-/*
- isEmpty
- count
- evenNumbers[index]
- 
- for (index,num) in evenNumbers.enumerated(){
-     print(index,num)
- }
- 
- */
+func printNameAndCity(dic : [String:String]){
+    if let name = dic["name"], let city = dic["city"]{
+        print(name, city)
+    }else{
+        print("-----> can not find")
+    }
+}
+
+printNameAndCity(dic : myDic)
